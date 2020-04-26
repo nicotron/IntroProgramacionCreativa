b86089acdbe7872eeebe79ef1ad24c459129a748
sekulpizarro <sekulpizarro@users.noreply.github.com>
2020-04-25 22:44:56 -0400

    Update sketch.js
    Doble check ortográfico y agregadas algunas lineas de explicación de la sintaxis.

--- pc1_ARBOL_1/sketch.js
+++ pc1_ARBOL_1/sketch.js
@@ -1,28 +1,31 @@
 /*
 contenido clase 1
 variables operadores condicionales
-ARBOL DE DESICION
+ARBOL DE DECISION
 */
 
-// varialbes globales: accesibles por todo el programa
+// variabLes globales: accesibles por todo el programa
+// se declaran antes del ambito (corchete)
+
 let a, c // declaracion
 let b = 2 // declaracion, inicialización de variable
 
 function setup() {
-    createCanvas(400, 400)
-    a = 2                                   // inicialización varaible numérica
-    c = true                                // inicialización variable booleana
+    createCanvas(400, 400) // creacion de ventana
+    a = 2  // inicializacion varaible numerica
+    c = true // inicializacion variable booleana
 
     // variable local: su uso no puede salir de su ambito {}
     let d = 0
 
-    // condicion:         si es verdadero ejecuta secuencia de
+    // condicion: si es verdadero ejecuta secuencia de
     // declaraciones dentro del ambito
+
     if (a > 1) { // uso de variable y operador aritmetico
         print('Verdadero')
         print(a > 1)
     } else { // si no es verdadero, se ejecuta las
-        print('Falso')                                  // siguientes líneas
+        print('Falso') // siguientes lineas
         print(a > 1)
     }
 
